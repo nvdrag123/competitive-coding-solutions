44270879beae727320f7168b2f128e9101ea75cf
nvdrag123 <33290971+nvdrag123@users.noreply.github.com>
2020-01-22 00:49:19 +0530

    Update CODECHEF DOOFSET.cpp

--- CODECHEF DOOFSET.cpp
+++ CODECHEF DOOFSET.cpp
@@ -90,10 +90,6 @@ void divide(int l,int r,int h){
 	return;
 }
 int main() {
-	#ifndef ONLINE_JUDGE
-			freopen("input.txt", "r", stdin);
-		    freopen("output.txt", "w", stdout);
-		#endif
 	ios_base::sync_with_stdio(0);cin.tie(0);
 	cin>>n>>m;
 	for(int i=2;i<mxN;i++)dp[i]=max(dp[i/2],dp[i-i/2])+1;   
